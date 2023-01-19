d4c29ea276500fe6ebfa85f9e8640554a55ba3e9
Terra-Credit <terracredit448@gmail.com>
2023-01-19 11:26:28 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -194,7 +194,7 @@ function App() {
       <s.Container
         flex={1}
         ai={"center"}
-        style={{ padding: 24, backgroundColor: "var(--primary)" }}
+        style={{ padding: 24, backgroundColor: "#000000" }}
         image={CONFIG.SHOW_BACKGROUND ? "/config/images/bg.png" : null}
       >
         <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
